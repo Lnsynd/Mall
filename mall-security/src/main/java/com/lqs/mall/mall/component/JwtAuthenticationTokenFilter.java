5248d9a063a06a8ac57373982943c2e53745baef
Lnsynd <2034215871@qq.com>
2023-07-02 13:37:16 +0800

    完善Jwt过滤器

--- mall-security/src/main/java/com/lqs/mall/mall/component/JwtAuthenticationTokenFilter.java
+++ mall-security/src/main/java/com/lqs/mall/mall/component/JwtAuthenticationTokenFilter.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -53,6 +54,11 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
             if(username!=null && SecurityContextHolder.getContext().getAuthentication() == null){
                 // 验证用户并返回
                 UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                // 检测token是否有效
+                if(jwtTokenUtil.validateToken(token,userDetails)){
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
             }
         }
         filterChain.doFilter(request,response);
