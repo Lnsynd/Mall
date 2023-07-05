f9e827e43deafe0b61fb8d458c19f1c0a3e37a4c
Lnsynd <2034215871@qq.com>
2023-07-05 17:40:01 +0800

    前台-订单定时任务开启

---
+++ mall-portal/src/main/java/com/lqs/mall/component/OrderTimeOutCancelTask.java
@@ -0,0 +1,28 @@
+package com.lqs.mall.component;
+
+import com.lqs.mall.service.OmsPortalOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * 订单超时后自动取消 并 解锁库存 的定时任务
+ * Created by 刘千山 on 2023/7/5/005-11:30
+ */
+public class OrderTimeOutCancelTask {
+    private final Logger LOGGER = LoggerFactory.getLogger(OrderTimeOutCancelTask.class);
+
+    @Autowired
+    private OmsPortalOrderService portalOrderService;
+
+    /**
+     * cron表达式：Seconds Minutes Hours DayofMonth Month DayofWeek [Year]
+     * 每10分钟扫描一次，扫描超时未支付订单，进行取消操作
+     */
+    @Scheduled(cron = "0 0/10 * ? * ?")
+    private void cancelTimeOutOrder(){
+        Integer count = portalOrderService.cancelTimeOutOrder();
+        LOGGER.info("取消订单，并根据sku编号释放锁定库存，取消订单数量：{}",count);
+    }
+}
---
+++ mall-portal/src/main/java/com/lqs/mall/config/SpringTaskConfig.java
@@ -0,0 +1,13 @@
+package com.lqs.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务相关配置
+ * Created by 刘千山 on 2023/7/5/005-11:30
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
